c06779d8303da9bf970a56c28e86c32c6a5d0b67
s3785167 <s3785167@student.rmit.edu.au>
2022-09-28 13:00:58 +1000

    Added dbrepository.js

---
+++ react/src/data/dbrepository.js
@@ -0,0 +1,34 @@
+//Methods to call the DB api from front end
+//Code adapted from week 8 lab repository.js
+
+//NOTE: kept the existing repository.js for now
+//to not break the app, but can replace
+//it with this one once we've fully moved from
+//local storage to db
+
+const API_HOST = "http://localhost:4000/api";
+const USER_KEY = "user";
+
+// --- User Methods ---------------------------------------------------------------------------------------
+async function verifyUser(email, password) {
+    const response = await axios.get(API_HOST + "/users/login", { params: { email, password } });
+    const user = response.data;
+    
+    // NOTE: In this example the login is also persistent as it is stored in local storage.
+    //if(user !== null)
+      //setUser(user);
+  
+    return user;
+}
+
+async function findUser(id) {
+    const response = await axios.get(API_HOST + `/users/select/${id}`);
+  
+    return response.data;
+}
+  
+async function createUser(user) {
+    const response = await axios.post(API_HOST + "/users", user);
+  
+    return response.data;
+}
