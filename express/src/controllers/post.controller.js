f173f016d8b09ecd9af0718de268847122c59f93
s3786643 <s3786643@student.rmit.edu.au>
2022-09-28 17:33:00 +1000

    Create post controller

---
+++ express/src/controllers/post.controller.js
@@ -0,0 +1,21 @@
+//Controller for calling methods on users
+//Adapted from week 8 Lab post.controller.js
+
+const db = require("../database");
+
+// Select all posts from the database.
+exports.all = async (req, res) => {
+  const posts = await db.post.findAll();
+  res.json(posts);
+};
+
+// Create a post in the database.
+exports.create = async (req, res) => {
+  const post = await db.post.create({
+    content: req.body.text,
+    image: req.body.image,
+    user_id: req.body.user_id
+  });
+
+  res.json(post);
+};
