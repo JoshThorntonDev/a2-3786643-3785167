e4db50cd44b9510125c29fb767162e0415522e4b
s3785167 <s3785167@student.rmit.edu.au>
2022-10-03 18:11:26 +1100

    commenting profile-test

--- react/src/tests/Profile.test.js
+++ react/src/tests/Profile.test.js
@@ -12,25 +12,32 @@ beforeAll(async () => {
 });
 
 
+//Test profile edit username function
 test("Change name", async () => {
+    //Get edit button and simulate click on it
     var button = screen.getAllByText('Edit');
     fireEvent.click(button[0])
 
+    //Get username edit field and update it
     var input = screen.getByPlaceholderText('First User')
     fireEvent.change(input, { target: { value: "New Name" } });
 
+    //Get verification password field and fill it in
     var input = screen.getByPlaceholderText('Enter your password here')
     fireEvent.change(input, { target: { value: "password1!" } });
 
     var button = screen.getByText('Save');
-    fireEvent.click(button)
 
+    //Simulate a click on submit button and wait for profile to update
+    fireEvent.click(button)
     await delay(3000)
     
+    //Assert that the new name is in the profile
     expect(screen.getAllByText("New Name", { exact: false })[0]).toBeInTheDocument();
 });
 
 
+//Function to log the user in before the test
 async function Login() {
     let button = screen.getAllByText("Login");
 
@@ -47,7 +54,6 @@ async function Login() {
     button = screen.getAllByText("Login");
     
     fireEvent.click(button[1])
-
     await delay(3000)
 
     expect(global.window.location.href).toContain('/profile/837')
