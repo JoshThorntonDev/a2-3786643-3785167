7c8e052125ce682cff955eccb1cde21afa3e2068
s3785167 <s3785167@student.rmit.edu.au>
2022-10-06 19:33:40 +1100

    WIP? Name change test

--- react/src/tests/Profile.test.js
+++ react/src/tests/Profile.test.js
@@ -1,50 +1,65 @@
 //Tests for profile page
 
 import { render, screen, fireEvent } from "@testing-library/react";
-import Profile from "../components/Profile.js";
-import UserContext from "../contexts/UserContext";
-import { BrowserRouter } from "react-router-dom";
+import App from "../App";
 
 let container;
-const currentUser = 2;
 
 beforeAll(async () => {
-    const utils = render(
-    <UserContext.Provider value={{ currentUser }}>
-        <BrowserRouter>
-            <Profile />
-        </BrowserRouter>
-    </UserContext.Provider>
-    );
+    const utils = render(<App />);
     container = utils.container;
+    await Login();
 });
 
 
 //Test profile edit username function
-test("Profile Name Change Form", async () => {
-    /*
+test("Change name", () => {
     //Get edit button and simulate click on it
     var button = screen.getAllByText('Edit');
     fireEvent.click(button[0])
 
     //Get username edit field and update it
-    var input = screen.getByPlaceholderText('First User')
-    fireEvent.change(input, { target: { value: "New Name" } });
+    const name = screen.getByPlaceholderText('Test User')
+    fireEvent.change(name, { target: { value: "New Name" } });
 
     //Get verification password field and fill it in
-    var input = screen.getByPlaceholderText('Enter your password here')
-    fireEvent.change(input, { target: { value: "password1!" } });
+    const password = screen.getByPlaceholderText('Enter your password here')
+    fireEvent.change(password, { target: { value: "password1!" } });
+
+    expect(name.value).toBe("New Name");
+    expect(password.value).toBe("password1!");
 
     var button = screen.getByText('Save');
 
     //Simulate a click on submit button and wait for profile to update
     fireEvent.click(button)
-    await delay(3000)
-    
-    //Assert that the new name is in the profile
-    expect(screen.getAllByText("New Name", { exact: false })[0]).toBeInTheDocument();
-    */
+
+    //Assert that "Updating" appears in the document after submitting
+    expect(screen.getByText("Updating")).toBeInTheDocument();
 });
+
+
+//Function to log the user in before the test
+async function Login() {
+    let button = screen.getAllByText("Login");
+
+    // Simulate click.
+    fireEvent.click(button[0]);
+
+    const email = screen.getByPlaceholderText('email@example.com');
+    fireEvent.change(email, { target: { value: "testuser@email.com" } });
+
+    const password = screen.getByPlaceholderText('Password here')
+    fireEvent.change(password, { target: { value: "password1!" } });
+
+
+    button = screen.getAllByText("Login");
+    
+    fireEvent.click(button[1])
+    await delay(3000)
+
+    expect(global.window.location.href).toContain('/profile/2')
+}
 
 function delay(milliseconds){
     return new Promise(resolve => {
