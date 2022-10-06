29e3da7b5ee270c57ffa269074f88074624fd35a
s3785167 <s3785167@student.rmit.edu.au>
2022-10-06 16:26:09 +1100

    Added unit test for login form

--- react/src/tests/Login.test.js
+++ react/src/tests/Login.test.js
@@ -20,9 +20,27 @@ beforeEach(() => {
         </UserContext.Provider>
     );
     container = utils.container;
-})
+});
 
 //Test the user login form
 test("Login form", () => {
+    //Get email and password form input elements
+    const email = screen.getByPlaceholderText("email@example.com");
+    const password = screen.getByPlaceholderText("Password here");
 
+    //Enter user details
+    fireEvent.change(email, { target: { value: "first@email.com" } });
+    fireEvent.change(password, { target: { value: "password1!" } });
+
+    //Expect input values to have been updated successfully
+    expect(email.value).toBe("first@email.com");
+    expect(password.value).toBe("password1!");
+
+    const button = screen.getByText("Login");
+
+    //Simulate click on submit button
+    fireEvent.click(button);
+
+    //Assert that "Loading" appears in the document after form is submitted
+    expect(screen.getByText("Loading")).toBeInTheDocument(); 
 });
