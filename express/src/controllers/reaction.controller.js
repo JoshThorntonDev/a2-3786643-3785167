45d4f7bacb61d641eb99edd54b97ae178c379364
s3786643 <s3786643@student.rmit.edu.au>
2022-10-06 18:42:22 +1100

    Define reaction controller

---
+++ express/src/controllers/reaction.controller.js
@@ -0,0 +1,38 @@
+const db = require("../database");
+
+exports.all = async (req, res) => {
+  const reactions = await db.reaction.findAll();
+  res.json(reactions);
+};
+
+// Create a reaction in the database.
+exports.create = async (req, res) => {
+  const reaction = await db.reaction.create({
+    type: req.body.type,
+    userId: req.body.userId,
+    postId: req.body.postId,
+  });
+
+  res.json(reaction);
+};
+
+exports.update = async (req, res) => {
+  const id = req.body.id;
+
+  const reaction = await db.reaction.findByPk(id);
+
+  reaction.type = req.body.type;
+
+  await reaction.save();
+  res.json(reaction);
+};
+
+// delete a reaction
+exports.delete = async (req, res) => {
+    const id = req.params.id;
+    
+    await db.reaction.destroy({where: {id: id}})
+    res.send({
+      message: `Deleted reaction ${id}`
+    });
+  }
