e75169094b6d534414d8d004250d35c7148c4a70
s3786643 <s3786643@student.rmit.edu.au>
2022-10-08 00:20:28 +1100

    Move test file

--- react/src/tests/ReactionArea.test.js
+++ react/src/tests/ReactionArea.test.js
@@ -1,5 +1,5 @@
 import { render, screen, fireEvent } from "@testing-library/react";
-import ReactionArea from "./ReactionArea";
+import ReactionArea from "../components/ReactionArea";
 
 let container;
 
