fea2cbbfe5b3dfb38c9931b683c29a5168da23a9
s3786643 <s3786643@student.rmit.edu.au>
2022-10-08 14:58:45 +1100

    Create functions for managing local post state

--- react/src/data/LocalPostManagement.js
+++ react/src/data/LocalPostManagement.js
@@ -1,8 +1,17 @@
-
-function getTopLevelPosts(localPosts) {
-
+function getTopLevelPosts(allLocalPosts) {
+  return allLocalPosts.filter((post) => post.depth === 0);
 }
 
-function getRepliesTo(localPost) {
-
+function getRepliesTo(allLocalPosts, postId) {
+  return allLocalPosts.filter((post) => post.replyId === postId);
 }
+
+function findPostsByUser(allLocalPosts, userId) {
+  return allLocalPosts.filter((post) => post.userId === userId);
+}
+
+function reversePosts(posts) {
+  return posts.reverse();
+}
+
+export { getTopLevelPosts, getRepliesTo, findPostsByUser, reversePosts };
