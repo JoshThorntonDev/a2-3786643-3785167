04cc9b701e76f85db65a7024975ae975ca2e40f7
s3786643 <s3786643@student.rmit.edu.au>
2022-10-09 17:26:20 +1100

    Put user info part of page into a component

--- react/src/components/Profile.js
+++ react/src/components/Profile.js
@@ -15,6 +15,7 @@ import Spinner from "react-bootstrap/Spinner";
 import { useNavigate } from "react-router-dom";
 import ReactPaginate from "react-paginate";
 import ReactionContext from "../contexts/ReactionContext";
+import ProfileCard from "./ProfileCard";
 
 function Profile() {
   const { id } = useParams();
@@ -78,27 +79,6 @@ function Profile() {
 
 
 
-  // the field is stored here to make it easier to clear the values when the modal is closed,
-  // either by closing it manually or when an update is successful
-
-  const [showEdit, setShowEdit] = useState(false); // state for profile edit modal
-  const [showDelete, setShowDelete] = useState(false); // state for profile delete modal
-
-  const toggleEdit = () => {
-    // toggle the edit state
-    // it also clears the fields in case the user opens it again
-    setShowEdit((current) => !current);
-  };
-
-  const toggleDelete = () => {
-    //toggle delete state, clear password in case it was entered before
-    setShowDelete((current) => !current);
-  };
-
-  const getDate = () => {
-    var date = new Date(user.createdAt);
-    return date.toLocaleDateString();
-  };
 
   const handlePageClick = (data) => {
     setPage(data.selected);
@@ -128,46 +108,7 @@ function Profile() {
         </div>
       ) : (
         <div>
-          <Card border="secondary" className="profile">
-            <ProfileEditor
-              show={showEdit}
-              toggle={toggleEdit}
-
-
-              user={user}
-              setUpdated={setUpdated}
-            />
-
-            {
-              <ProfileDeleter
-                show={showDelete}
-                toggle={toggleDelete}
-
-                user={user}
-              />
-            }
-            <PersonCircle size={"100"} className="image"></PersonCircle>
-
-            <div className="information">
-              <h1>{user.username}'s Profile</h1>
-              <p>{user.email}</p>
-              <hr />
-              <p>Joined: {getDate()}</p>
-            </div>
-
-            {isThisMyAccount && ( // only show edit and delete when its the logged in account
-              <div className="edit">
-                <Button onClick={toggleEdit} variant="primary" type="submit">
-                  <PencilSquare size={"20"}></PencilSquare> Edit
-                </Button>
-
-                <Button onClick={toggleDelete} variant="danger" type="submit">
-                  <Trash size={"20"}></Trash> Delete
-                </Button>
-              </div>
-            )}
-          </Card>
-
+            <ProfileCard user={user} isThisMyAccount={isThisMyAccount}/>
           <hr />
 
           <h3>Posts by {user.username}</h3>
