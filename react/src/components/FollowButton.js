ddcb5f21adac5b708231f76397a5c99563274f21
s3786643 <s3786643@student.rmit.edu.au>
2022-10-14 16:18:57 +1100

    Disable follow button before fully loading

--- react/src/components/FollowButton.js
+++ react/src/components/FollowButton.js
@@ -2,7 +2,14 @@ import Button from "react-bootstrap/Button";
 import { PersonPlusFill, PersonDashFill } from "react-bootstrap-icons";
 import { useContext, useEffect, useState } from "react";
 import UserContext from "../contexts/UserContext";
-import { createFollow, deleteFollow, findFollowedUsers, getFollows } from "../data/dbrepository.js";
+import {
+  createFollow,
+  deleteFollow,
+  findFollowedUsers,
+  getFollows,
+} from "../data/dbrepository.js";
+
+
 
 function FollowButton(props) {
   const { currentUser } = useContext(UserContext);
@@ -15,9 +22,13 @@ function FollowButton(props) {
       const follows = await findFollowedUsers(Number(currentUser));
 
       //if the user's id is found, set following to true
-      if(follows.find(
-        (follow) => follow.userId === Number(currentUser) && follow.followingId === props.userId
-      )) {
+      if (
+        follows.find(
+          (follow) =>
+            follow.userId === Number(currentUser) &&
+            follow.followingId === props.userId
+        )
+      ) {
         setFollowing(true);
       }
     }
@@ -34,32 +45,47 @@ function FollowButton(props) {
     //Call createFollow method which saves the follow details to db
     await createFollow(follow);
 
-    setFollowing(true)
+    setFollowing(true);
   };
 
   const unfollowUser = async () => {
-    const follows = await findFollowedUsers(Number(currentUser))
+    const follows = await findFollowedUsers(Number(currentUser));
 
     // find the follow that relates to the current user and this particular button
     var temp = follows.find(
-      (follow) => follow.userId === Number(currentUser) && follow.followingId === props.userId
+      (follow) =>
+        follow.userId === Number(currentUser) &&
+        follow.followingId === props.userId
     );
 
     await deleteFollow(temp.id);
-      setFollowing(false)
+    setFollowing(false);
   };
-
 
   return (
     <div>
-      {following ? (
-        <Button variant="outline-danger" type="submit" onClick={unfollowUser}>
-          <PersonDashFill size={20} /> Unfollow
+      {following === undefined ? (
+        <Button variant="outline-secondary" disabled>
+          {" "} 
+          <PersonPlusFill size={20} /> {" "}
         </Button>
       ) : (
-        <Button variant="primary" type="submit" onClick={followUser}>
-          <PersonPlusFill size={20} /> Follow
-        </Button>
+        <div>
+          {" "}
+          {following ? (
+            <Button
+              variant="outline-danger"
+              type="submit"
+              onClick={unfollowUser}
+            >
+              <PersonDashFill size={20} /> Unfollow
+            </Button>
+          ) : (
+            <Button variant="primary" type="submit" onClick={followUser}>
+              <PersonPlusFill size={20} /> Follow
+            </Button>
+          )}
+        </div>
       )}
     </div>
   );
