5ab33ca957157b8ba406ed082aaa7a07150f5e43
s3786643 <s3786643@student.rmit.edu.au>
2022-10-14 12:07:05 +1100

    Make follow change based on a boolean

--- react/src/components/FollowButton.js
+++ react/src/components/FollowButton.js
@@ -1,29 +1,50 @@
 import Button from "react-bootstrap/Button";
-import { PersonPlusFill } from "react-bootstrap-icons";
-import { useContext } from "react";
+import { PersonPlusFill, PersonDashFill } from "react-bootstrap-icons";
+import { useContext, useEffect, useState } from "react";
 import UserContext from "../contexts/UserContext";
-import { createFollow } from "../data/dbrepository.js";
+import { createFollow, deleteFollow } from "../data/dbrepository.js";
 
 function FollowButton(props) {
-
   const { currentUser } = useContext(UserContext);
 
-  const followUser = async () => {
+  const [following, setFollowing] = useState(props.default);
 
+  const followUser = async () => {
     //Store the id of the user to be followed, and the current user
     const follow = {
       followingId: props.userId,
-      userId: currentUser
+      userId: currentUser,
     };
 
     //Call createFollow method which saves the follow details to db
     await createFollow(follow);
   };
 
+  const unfollowUser = async () => {
+
+    const follow = {
+      followingId: props.userId,
+      userId: currentUser,
+    };
+
+
+    await deleteFollow(follow);
+
+  };
+
+
   return (
-    <Button variant="primary" type="submit" onClick={followUser}>
-      <PersonPlusFill size={20} /> Follow
-    </Button>
+    <div>
+      {following ? (
+        <Button variant="outline-danger" type="submit" onClick={unfollowUser}>
+          <PersonDashFill size={20} /> Unfollow
+        </Button>
+      ) : (
+        <Button variant="primary" type="submit" onClick={followUser}>
+          <PersonPlusFill size={20} /> Follow
+        </Button>
+      )}
+    </div>
   );
 }
 
