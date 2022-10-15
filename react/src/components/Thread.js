b89a13850ca3b096c4fde08902a830a4721316d1
s3786643 <s3786643@student.rmit.edu.au>
2022-10-15 16:54:50 +1100

    Reduce stutter of post display animation

--- react/src/components/Thread.js
+++ react/src/components/Thread.js
@@ -26,7 +26,11 @@ function Thread(props) {
     }
 
     getReplies();
-    setShowSelf(true);
+
+    setTimeout(() => { // prevents the collapse animation stuttering because the height of the page changes as more posts render
+      setShowSelf(true);
+    }, 300)
+
     setShowReplies(true);
   }, [post.id]);
 
